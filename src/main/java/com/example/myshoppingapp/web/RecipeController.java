5d8389b25bb8ab2a07eb3fdec49dbdaa6b16f82c
MartinMenev <martin.menew@gmail.com>
2023-03-02 07:34:48 +0200

    Fixed pagination logic (do not show pagination if we have only one page)

--- src/main/java/com/example/myshoppingapp/web/RecipeController.java
+++ src/main/java/com/example/myshoppingapp/web/RecipeController.java
@@ -65,7 +65,7 @@ public class RecipeController {
         @PageableDefault(
                 sort = "id",
                 direction = Sort.Direction.DESC,
-                size = 2) Pageable pageable) {
+                size = 9) Pageable pageable) {
             model.addAttribute("recipes", recipeService.showAllRecipes(pageable));
 
             return "recipe/all-recipes";
