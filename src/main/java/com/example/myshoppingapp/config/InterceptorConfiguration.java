a9025f07b4da56fe1445272946cab746c192a8dd
MartinMenev <martin.menew@gmail.com>
2023-03-17 17:28:56 +0200

    Added Language Interceptor logic

--- src/main/java/com/example/myshoppingapp/config/InterceptorConfiguration.java
+++ src/main/java/com/example/myshoppingapp/config/InterceptorConfiguration.java
@@ -1,16 +1,29 @@
 package com.example.myshoppingapp.config;
 
 import com.example.myshoppingapp.interceptor.LoggingInterceptor;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
+
+    private final LocaleChangeInterceptor localeChangeInterceptor;
+
+    public InterceptorConfiguration(LocaleChangeInterceptor localeChangeInterceptor) {
+        this.localeChangeInterceptor = localeChangeInterceptor;
+    }
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggingInterceptor());
             super.addInterceptors(registry);
+        registry.addInterceptor(localeChangeInterceptor);
+
     }
+
+
+
 }
