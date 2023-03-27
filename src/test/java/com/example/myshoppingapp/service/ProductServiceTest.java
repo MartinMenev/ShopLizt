aad00e1dbb3fe37d6a86671f7128ed3b599864b3
MartinMenev <martin.menew@gmail.com>
2023-03-27 08:50:00 +0300

    new Unit tests

--- src/test/java/com/example/myshoppingapp/service/ProductServiceTest.java
+++ src/test/java/com/example/myshoppingapp/service/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.myshoppingapp.service;
 
+import com.example.myshoppingapp.exceptions.ObjectNotFoundException;
 import com.example.myshoppingapp.model.products.InputProductDTO;
 import com.example.myshoppingapp.model.products.OutputProductDTO;
 import com.example.myshoppingapp.model.products.Product;
@@ -8,16 +9,23 @@ import com.example.myshoppingapp.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -66,7 +74,7 @@ class ProductServiceTest {
     @Test
     public void testToGetAllProducts() {
         List<Product> testList = new ArrayList<>();
-        testProduct.setPosition(1);
+        testProduct.setPosition(1L);
         testList.add(testProduct);
         when(mockUserService.getLoggedUserId(testUser.getUsername())).thenReturn(testUser.getId());
         when(mockProductRepository.findAllByUserEntityId(testUser.getId())).thenReturn(Optional.of(testList));
@@ -81,7 +89,75 @@ class ProductServiceTest {
 
     }
 
+    @Test
+    public void testToUpdateProduct() {
+        InputProductDTO testInputProduct = modelMapper.map(testProduct, InputProductDTO.class);
+        testInputProduct.setName("I am a new product name");
 
+        assertEquals(testInputProduct.getName(), toTest.updateProduct(testInputProduct));
+        verify(mockProductRepository).updateProductName(testInputProduct.getId(), testInputProduct.getName());
+    }
+
+
+    @Test
+    public void testToDeleteProduct() {
+        assertEquals(testProduct.getId(), toTest.deleteById(testProduct.getId()));
+
+    }
+
+    @Test
+    public void testToGetProductById() {
+        when(mockProductRepository.getProductById(testProduct.getId())).thenReturn(Optional.of(testProduct));
+
+        OutputProductDTO testProductOutputDTO = modelMapper.map(testProduct, OutputProductDTO.class);
+
+        assertEquals(testProductOutputDTO.getName(), toTest.getProductById(testProduct.getId()).getName());
+        assertEquals(testProductOutputDTO.getId(), toTest.getProductById(testProduct.getId()).getId());
+
+        assertThrows(ObjectNotFoundException.class,
+                () -> toTest.getProductById(113L));
+    }
+
+    @Test
+    public void testToBuyProduct() {
+        when(mockProductRepository.getProductById(testProduct.getId())).thenReturn(Optional.of(testProduct));
+        when(mockUserService.getLoggedUser(testUser.getUsername())).thenReturn(testUser);
+
+        assertEquals(testUser.getUsername(), toTest.buyProduct(testProduct.getId(), testUser.getUsername()).getUsername());
+
+        assertThrows(NoSuchElementException.class,
+                ()-> toTest.buyProduct(113L, testUser.getUsername()));
+    }
+
+    @Test
+    public void testToShowBoughtProducts() {
+        List<OutputProductDTO> testOutputList = new ArrayList<>();
+
+        OutputProductDTO testOutputProduct = modelMapper.map(testProduct, OutputProductDTO.class);
+        testOutputList.add(testOutputProduct);
+        Product product = modelMapper.map(testOutputProduct, Product.class);
+        List<Product> testProductList = new ArrayList<>();
+        testProductList.add(product);
+
+        when(mockUserService.getLoggedUserId(testUser.getUsername())).thenReturn(testUser.getId());
+        when(mockProductRepository.findAllByBuyerId(testUser.getId())).thenReturn(Optional.of(testProductList));
+
+        assertEquals(testOutputList.size(), toTest.showBoughtProducts(testUser.getUsername()).size());
+        assertEquals(testOutputList.get(0).getId(), toTest.showBoughtProducts(testUser.getUsername()).get(0).getId());
+
+    }
+
+    @Test
+    public void testToReuseProduct() {
+        testProduct
+                .setBuyer(testUser);
+
+        when(mockProductRepository.getProductById(testProduct.getId())).thenReturn(Optional.of(testProduct));
+
+        toTest.reuseProduct(testProduct.getId());
+        assertNull(testProduct.getBuyer());
+
+    }
 
 
 }
